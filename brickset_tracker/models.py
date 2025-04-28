ad23c854c6eadc41b5a21c39725805b47d79fc46
Lippai Róbert <robertlippai25@gmail.com>
2025-04-28 22:43:01 +0200

    Added tags, minifigs count, and instruction URL to model and details view

--- brickset_tracker/models.py
+++ brickset_tracker/models.py
@@ -1,11 +1,17 @@
 from flask_login import UserMixin
 from app import db
 
-# This is the association table
+# Association tables
 user_sets = db.Table('user_sets',
     db.Column('user_id', db.Integer, db.ForeignKey('users.uid'), primary_key=True),
     db.Column('set_id', db.Integer, db.ForeignKey('sets.sid'), primary_key=True)
 )
+
+sets_tags = db.Table('sets_tags',
+    db.Column('set_id', db.Integer, db.ForeignKey('sets.sid'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+)
+
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -21,7 +27,12 @@ class User(db.Model, UserMixin):
     
     def get_id(self):
         return self.uid
-    
+
+class TagModel(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+   
 class SetModel(db.Model):
     __tablename__ = 'sets'
     sid = db.Column(db.Integer, primary_key=True)
@@ -31,12 +42,18 @@ class SetModel(db.Model):
     year = db.Column(db.Integer)
     image_url = db.Column(db.String(200)) # /static/images/set_image.jpg
     description = db.Column(db.Text)
+    
+    instruction_url = db.Column(db.String(300))
+    minifigures = db.Column(db.Integer)
+
 
     # 1 to many
     brand_id = db.Column(db.Integer, db.ForeignKey('brands.bid'), nullable=False)
+    # many to many
+    tags = db.relationship('TagModel', secondary=sets_tags, backref='sets', lazy=True)
     
     # later store who added the set?
-    # created_by = db.Column(db.Integer, db.ForeignKey('user.uid'))
+    # created_by = db.Column(db.Integer, db.ForeignKey('users.uid'))
 
 class BrandModel(db.Model):
     __tablename__ = 'brands'
